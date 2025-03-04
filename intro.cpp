25f50a0aa589304c6827fc8d879b01d64ec6081f
blqies20 <balqis1maharani@gmail.com>
2025-03-04 12:06:58 +0700

    introduction c++

---
+++ intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char jenisKelamin = 'P';
+    string jenis = "perempuan";
+    float x = 10.5;
+    int y = 5;
+    bool check = true;
+
+    cout << "Selamat datang di TI UMY\n";
+    cout << "Yogyakarta" << endl;
+    cout << "Indonesia";
+}
